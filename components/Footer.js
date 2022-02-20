e7340c857bfccceef398b3734e5be038ad7a1c33
RubenF01 <51177012+RubenF01@users.noreply.github.com>
2022-02-20 16:36:08 -0400

    Added last hover effects

--- components/Footer.js
+++ components/Footer.js
@@ -1,66 +1,107 @@
-import ClassGroup from 'classgroup';
+import ClassGroup from "classgroup";
 
 const Footer = () => {
- return (
-  <div className={classes.footerContainer}>
-   <div className={classes.topFooter}>
-    <img src="/logo.svg" alt="logo" />
-    <div className={classes.socialContainer}>
-     <img className={classes.socialIcon} src="/icon-facebook.svg" alt="facebook" />
-     <img className={classes.socialIcon} src="/icon-twitter.svg" alt="twitter" />
-     <img className={classes.socialIcon} src="/icon-pinterest.svg" alt="pinterest" />
-     <img className={classes.socialIcon} src="/icon-instagram.svg" alt="instagram" />
-    </div>
-   </div>
+  return (
+    <div className={classes.footerContainer}>
+      <div className={classes.topFooter}>
+        <img src="/logo.svg" alt="logo" />
+        <div className={classes.socialContainer}>
+          <div className={classes.iconContainer}>
+            <img
+              className={classes.socialIcon}
+              src="/icon-facebook.svg"
+              alt="facebook"
+            />
+          </div>
+          <div className={classes.iconContainer}>
+            <img
+              className={classes.socialIcon}
+              src="/icon-twitter.svg"
+              alt="twitter"
+            />
+          </div>
+          <div className={classes.iconContainer}>
+            <img
+              className={classes.socialIcon}
+              src="/icon-pinterest.svg"
+              alt="pinterest"
+            />
+          </div>
+          <div className={classes.iconContainer}>
+            <img
+              className={classes.socialIcon}
+              src="/icon-instagram.svg"
+              alt="instagram"
+            />
+          </div>
+        </div>
+      </div>
 
-   <div className={classes.botFooter}>
-    <div className={classes.botNav}>
-     <a className={classes.navElement} href="#">About</a>
-     <a className={classes.navElement} href="#">Careers</a>
-     <a className={classes.navElement} href="#">Events</a>
-     <a className={classes.navElement} href="#">Products</a>
-     <a className={classes.navElement} href="#">Support</a>
+      <div className={classes.botFooter}>
+        <div className={classes.botNav}>
+          <a className={classes.navElement} href="#">
+            About
+          </a>
+          <a className={classes.navElement} href="#">
+            Careers
+          </a>
+          <a className={classes.navElement} href="#">
+            Events
+          </a>
+          <a className={classes.navElement} href="#">
+            Products
+          </a>
+          <a className={classes.navElement} href="#">
+            Support
+          </a>
+        </div>
+        <p className={classes.copyright}>
+          &copy; 2022 Loopstudios. All rights reserved.
+        </p>
+      </div>
     </div>
-    <p className={classes.copyright}>&copy; 2022 Loopstudios. All rights reserved.</p>
-   </div>
-  </div>
- )
-}
+  );
+};
 
 export default Footer;
 
 const classes = ClassGroup({
- footerContainer: {
-  decoration: "bg-black py-10"
- },
+  footerContainer: {
+    decoration: "bg-black py-10",
+  },
 
- topFooter: {
-  layout: "flex w-full justify-between px-80 pb-5",
- },
+  topFooter: {
+    layout: "flex w-full justify-between px-80 pb-5",
+  },
 
- socialContainer: {
-  layout: "flex"
- },
+  socialContainer: {
+    layout: "flex",
+  },
 
- botFooter: {
-  layout: "w-full flex justify-between px-80",
-  decoration: "text-white font-font1"
- },
+  iconContainer: {
+    content:
+      "hover:after:content-[''] hover:after:bg-white hover:after:h-px hover:after:w-6 hover:after:absolute hover:relative hover:after:z-10 hover:after:bottom-0 flex justify-center",
+  },
 
- botNav: {
-  layout: "flex gap-x-8"
- },
+  botFooter: {
+    layout: "w-full flex justify-between px-80",
+    decoration: "text-white font-font1",
+  },
 
- navElement: {
-  content: "hover:after:content-[''] hover:after:bg-white hover:after:h-px hover:after:w-6 hover:after:absolute flex justify-center relative after:z-10 hover:after:bottom-[-10px]"
- },
+  botNav: {
+    layout: "flex gap-x-8",
+  },
 
- socialIcon: {
-  layout: "h-6 px-2 cursor-pointer",
-  content: "after:content-[''] after:bg-white after:h-px after:w-6 after:absolute flex justify-center relative after:z-10 after:bottom-[-10px]"
- },
+  navElement: {
+    content:
+      "hover:after:content-[''] hover:after:bg-white hover:after:h-px hover:after:w-6 hover:after:absolute flex justify-center relative after:z-10 hover:after:bottom-[-10px]",
+  },
 
- copyright: {
-  decoration: "text-veryDarkGray"
- },
+  socialIcon: {
+    layout: "h-6 px-2 cursor-pointer",
+  },
+
+  copyright: {
+    decoration: "text-veryDarkGray",
+  },
 });
